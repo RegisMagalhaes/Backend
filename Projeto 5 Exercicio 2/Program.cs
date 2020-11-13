64949e4cf2b81d26f419adf518454a0e3283b6e9
DEV <dev@info.local>
2020-11-13 16:36:25 -0300

    Exercicios feitos em sala de aula

---
+++ Projeto 5 Exercicio 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Projeto_5_Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Em que ano estamos: ");
+            int anoatual = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Em que ano você nasceu: ");
+            int anonasc = int.Parse(Console.ReadLine());
+
+            int idade = anoatual - anonasc;
+            int semana =idade * 48;
+
+            Console.WriteLine("Você possui " + idade + " anos de idade e já viveu " + semana + " semanas.");
+        }
+    }
+}

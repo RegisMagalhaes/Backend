1eec7a51306ecb017e799ee0b0407c22925f6c50
DEV <dev@info.local>
2020-11-13 16:38:44 -0300

    Exercicio 1 feito em sala

---
+++ Projeto 4 Exercicio 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Projeto_4_Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            int meses = idade * 12;
+            int dias = meses * 360;
+            int horas = dias * 24;
+            int minutos = horas * 60;
+
+            Console.WriteLine("Você já viveu: " + meses + " meses, "+ dias + " dias, " + horas + " horas,  " + minutos + " minutos.");
+        }
+    }
+}

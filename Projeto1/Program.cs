8ee7c1197ce7788f5d39c3ebc12057ea342ebd35
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-13 00:39:33 -0300

    Projeto 1 Console Realizado

---
+++ Projeto1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int soma = num1 + num2;
+            int menos = num1 - num2;
+            int vezes = num1 * num2;
+            int divide = num1 / num2;
+
+            Console.WriteLine("O resultado da soma é:" + soma);
+            Console.WriteLine("A subtração é: "+ menos);
+            Console.WriteLine("A Multiplicação é:"+ vezes);
+            Console.WriteLine("A divisão é: "+ divide);
+
+        }
+    }
+}
